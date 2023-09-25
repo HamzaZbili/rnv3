d954b63be8e04d7ac35df93ad67da7c92fc9210c
Hamza Zbili <hamza.zbili.92@gmail.com>
2023-09-25 19:18:08 +0200

    added scroll to top

--- src/app/archive/page.tsx
+++ src/app/archive/page.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import { archiveData } from "@/app/archive/archiveData";
-import Link from "next/link";;
+import Link from "next/link";
 
 const page = () => {
   return (
@@ -41,8 +42,47 @@ const page = () => {
           );
         })}
       </ul>
+      <ScrollToTop />
     </section>
   );
 };
 
 export default page;
+
+function ScrollToTop() {
+
+ const [scrollY, setScrollY] = useState(0);
+  const scroll = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  useEffect(()=>{
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  },[])
+
+  return (
+    <div
+      className={` fixed bottom-[2rem] right-[2rem] bg-bone rounded-full transition-opacity duration-300 
+    ${scrollY > 700 ? "opacity-80 w-10 h-10" : "opacity-0 h-0 w-0"}`}
+    >
+      <div
+        className="flex justify-center items-center z-50
+            rounded-md cursor-pointer "
+        onClick={scroll}
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+          <g data-name="90-Arrow Up">
+            <path d="M16 0a16 16 0 1 0 16 16A16 16 0 0 0 16 0zm0 30a14 14 0 1 1 14-14 14 14 0 0 1-14 14z" />
+            <path d="m15.29 10.29-8 8L8.7 19.7l7.3-7.29 7.29 7.29 1.41-1.41-8-8a1 1 0 0 0-1.41 0z" />
+          </g>
+        </svg>
+      </div>
+    </div>
+  );
+}
